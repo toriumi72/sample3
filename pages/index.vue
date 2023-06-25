4ec757f3102afcf4d370041e370c2d6ebe650869
nekonote <User2@nekonote.local>
2023-06-26 00:15:37 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const items = ref<any>()
+
+const onGetItems = async () => {
+  const response = await useFetch('https://qiita.com/api/v2/items')
+  items.value = response.data._rawValue
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      API-test-app
+    </div>
+    <div>
+      <button @click="onGetItems">
+        取得する
+      </button>
+    </div>
+    <div v-for="(item, index) in items" :key="index" class="flex">
+      <div>
+        {{ index + 1 }}
+      </div>
+      <div>
+        <nuxt-link :to="item.url">
+          {{ item.title }}
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+
